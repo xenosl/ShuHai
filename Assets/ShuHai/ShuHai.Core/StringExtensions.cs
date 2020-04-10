fc494a0d6d3597259785bbcee2cee0d8535f880b
xenosl <xenolsy@hotmail.com>
2020-04-10 23:46:34 +0800

    Extension methods for convert encoding of string.

--- Assets/ShuHai/ShuHai.Core/StringExtensions.cs
+++ Assets/ShuHai/ShuHai.Core/StringExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace ShuHai
 {
@@ -26,5 +27,34 @@ namespace ShuHai
         {
             return self.IndexOf(value, comparison) >= 0;
         }
+
+        #region Encoding
+
+        public static string ConvertEncoding(this string self, string srcEncodingName, string dstEncodingName)
+        {
+            var src = Encoding.GetEncoding(srcEncodingName);
+            var dst = Encoding.GetEncoding(dstEncodingName);
+            return ConvertEncoding(self, src, dst);
+        }
+
+        public static string ConvertEncoding(this string self, int srcCodePage, int dstCodePage)
+        {
+            var src = Encoding.GetEncoding(srcCodePage);
+            var dst = Encoding.GetEncoding(dstCodePage);
+            return ConvertEncoding(self, src, dst);
+        }
+
+        public static string ConvertEncoding(this string self, Encoding srcEncoding, Encoding dstEncoding)
+        {
+            Ensure.Argument.NotNull(self, nameof(self));
+            Ensure.Argument.NotNull(srcEncoding, nameof(srcEncoding));
+            Ensure.Argument.NotNull(dstEncoding, nameof(dstEncoding));
+
+            var srcBytes = srcEncoding.GetBytes(self);
+            var dstBytes = Encoding.Convert(srcEncoding, dstEncoding, srcBytes);
+            return dstEncoding.GetString(dstBytes);
+        }
+
+        #endregion Encoding
     }
 }
