bcfd009f26ad7ac3704fa557bfd6d94e0b648e10
xenosl <xenolsy@hotmail.com>
2021-03-22 21:39:40 +0800

    UI.text extension for localization.

---
+++ Packages/ShuHai.I18N.Unity.UI/Runtime/TextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+
+namespace ShuHai.I18N.Unity.UI
+{
+    public static class TextExtensions
+    {
+        public static void LocalizeText(this Text self, Language language, string key)
+        {
+            Ensure.Argument.NotNull(language, nameof(language));
+            self.text = language.GetText(key);
+        }
+
+        public static void LocalizeText(this Text self, string key) { LocalizeText(self, Language.Current, key); }
+    }
+}
