16926af001022ae2d58a8ba95628bea84b86ee49
xenosl <xenolsy@hotmail.com>
2021-03-24 17:39:38 +0800

    Text localizer for i18N.

---
+++ Packages/ShuHai.I18N.Unity.UI/Runtime/TextLocalizer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ShuHai.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShuHai.I18N.Unity.UI
+{
+    [RequireComponent(typeof(Text))]
+    public class TextLocalizer : MonoBehaviour
+    {
+        protected TextLocalizer() { _text = new Lazy<Text>(EnsureText); }
+
+        #region Text Key
+
+        public string TextKey
+        {
+            get => _textKey;
+            set
+            {
+                if (value == _textKey)
+                    return;
+                _textKey = value;
+                ApplyTextKey();
+            }
+        }
+
+        [SerializeField]
+        private string _textKey;
+
+        private void ApplyTextKey() { Text.text = Language.Current.GetText(_textKey); }
+
+        #endregion Text Key
+
+        #region Text Component
+
+        public Text Text => _text.Value;
+        private readonly Lazy<Text> _text;
+
+        private Text EnsureText() { return gameObject.EnsureComponent<Text>(); }
+
+        #endregion Text Component
+
+        #region Unity Events
+
+#if UNITY_EDITOR
+        private void OnValidate() { ApplyTextKey(); }
+#endif
+
+        #endregion Unity Events
+    }
+}
