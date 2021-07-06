390afbd38ac732dd3f27d2f843d7ee2faf204179
xenol <xenolsy@hotmail.com>
2021-07-06 22:05:23 +0800

    add ChangeEventArgs.

---
+++ Source/ShuHai.Core/ChangeEvent.cs
@@ -0,0 +1,31 @@
+﻿namespace ShuHai
+{
+    public class ChangeEventArgs<T>
+    {
+        public T PreviousValue { get; }
+
+        public T CurrentValue { get; }
+
+        public ChangeEventArgs(T previousValue, T currentValue)
+        {
+            PreviousValue = previousValue;
+            CurrentValue = currentValue;
+        }
+    }
+
+    public class ChangeEventArgs<TSender, TValue>
+    {
+        public TSender Sender { get; }
+
+        public TValue PreviousValue { get; }
+
+        public TValue CurrentValue { get; }
+
+        public ChangeEventArgs(TSender sender, TValue previousValue, TValue currentValue)
+        {
+            Sender = sender;
+            PreviousValue = previousValue;
+            CurrentValue = currentValue;
+        }
+    }
+}
